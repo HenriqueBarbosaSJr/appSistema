885ca609715b396c38d0635e171b669c4c758ab3
Henrique Barbosa de Souza Júnior <henrique@hbveterinaria.com.br>
2024-10-21 21:13:12 -0300

    Consulta de dados

---
+++ src/funcionarios/funcionarios.js
@@ -0,0 +1,35 @@
+import funcionarios from '../../databases/banco.js';
+
+// Referências do DOM HTM
+const tbodyList = document.getElementById('tbodyList');
+
+
+//Lógica
+
+export async function consultaAPI(){
+    console.log('consultando...');
+    const response = await funcionarios;
+    atualizarTabela(response.banco);
+    return response.banco
+}
+
+
+export function atualizarTabela(dados){
+    console.log(dados);
+    let rows='';
+    for (let i=0; i < dados.length; i++){
+        let tr = '<tr>' +
+                        '<td>' + dados[i].id + '</td>' +
+                        '<td>' + dados[i].nome + '</td>' +
+                        '<td>' + dados[i].departamento + '</td>' +
+                        '<td>' + dados[i].funcao + '</td>' +
+                        '<td>' + dados[i].salario + '</td>' +
+                        '<td>' +
+                            '<a id="btnTrash" onclick="deletePro(this)"><img src="../assets/trash.png" class="icons"></a>' +
+                            '<a id="btnEdit" onclick="alteraPro(this)"><img src="../assets/edit2.png" class="icons"></a>' +
+                        '</td>' +
+                  '</tr>'
+        rows += tr;   
+    }
+    tbodyList.innerHTML = rows
+}
--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
-// Referências do DOM HTML
+import { consultaAPI } from './funcionarios/funcionarios.js';
 
+
+// Referências do DOM HTML
 const btnIncluir = document.getElementById('btnIncluir');
 const modalProdutos = document.getElementById('modalProdutos');
 const btnFecharModal = document.getElementById('btnFecharModal');
 
 // Lógica
+
+consultaAPI();
 
 btnIncluir.onclick = ()=>{
     modalProdutos.show();
